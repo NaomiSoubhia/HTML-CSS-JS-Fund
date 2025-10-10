dcef40df430c7b195fbaef0f33551f8359a3bc00
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2025-10-10 12:42:05 -0400

    Adjust

--- lesson-6/includes/nav.php
+++ lesson-6/includes/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="/lesson-6/index.php">Home/a></li>
+        <li><a href="/lesson-6/index.php">Home</a></li>
         <li><a href="/lesson-6/red.php">Red Blocks</a></li>
         <li><a href="/lesson-6/blue.php">Blue Blocks</a></li>
         <li><a href="/lesson-6/yellow.php">Yellow Blocks</a></li>
